bea23e9da8a1768d72bdf4c932e3d54c8948241b
venkatchinmai <venkatachinmai@gmail.com>
2019-05-21 23:26:44 -0400

    Task1 Completed

---
+++ Task1.py
@@ -0,0 +1,13 @@
+class Add:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+class Mul:
+    def __init__(self,num3,num4):
+        self.num3=num3
+        self.num4=num4
+class Num:
+    def __init__(self,num5):
+        self.num5=num5
+a=Num(5)
+print(a.num5)
