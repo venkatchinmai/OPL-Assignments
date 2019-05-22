33929c9e0ac99876db6d0e397a2bdb281cb1d28b
venkatchinmai <venkatachinmai@gmail.com>
2019-05-22 19:30:17 -0400

    Task2 Completed

--- Task2.py
+++ Task2.py
@@ -1,53 +1,21 @@
-class Add:
-    def __init__(self,num1,num2):
-        self.num1=num1
-        self.num2=num2
-    def res(self):
-        print("(+" +str(self.num1)+" " +self.res(self.num2)+")")
-       
+class add:
+    def __init__(self,number1,number2):
+        self.number1=number1
+        self.number2=number2
+    def __str__(self):
+        return "(+ "+str(self.number1)+" "+str(self.number2)+" )"
+class mul:
+    def __init__(self,number3,number4):
+        self.number3=number3
+        self.number4=number4
+    def __str__(self):
+        return "(* "+str(self.number3)+" "+str(self.number4)+" )"
+class num:
+    def __init__(self,number5):
+        self.number5=number5
+    def __str__(self):
+        return str(self.number5)
 
-class Mul:
-    def __init__(self,num3,num4):
-        self.num3=num3
-        self.num4=num4
-    def multiply(self):
-        print("(*" +str(self.num3) +" "+str(self.num4)+")")
-        
-class Num:
-    def __init__(self,num5):
-        self.num5=num5
-    def number(self):
-        print(str(self.num5))
-
-
-
-a = Add(2,Add(3,4))
-a.res()
-m = Mul(5,6)
-#m.multiply()
-
-
-
-
-
-
-
-# a1=input("Enter number")
-# a2=input("Enter number")
-# sign=input("enter sign")
-# if(sign=="+"):
-#     a1=Add(a1,a2)
-#     a1.sum()
-# else:
-#     m1=Mul(a1,a2)
-#     m1.multiply()
-
-# n1=Num(50)
-# n1.number()
-
-
-
-
-
-
+final=add(num(3),mul(num(6),add(num(7),num(8))))
+print(final)
 
