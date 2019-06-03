6afa90a00103857b36eea5d85d735c674ac31f7a
Chinmai Sai <venkatachinmai@gmail.com>
2019-06-03 00:29:06 -0400

    Completed Task4

---
+++ Task4.py
@@ -0,0 +1,29 @@
+class add:
+    def __init__(self,number1,number2):
+        self.number1=number1
+        self.number2=number2
+     
+    def prettyprint(self):
+        result=self.number1.prettyprint() + self.number2.prettyprint()
+        return result
+        
+class mul:
+    def __init__(self,number3,number4):
+        self.number3=number3
+        self.number4=number4
+        
+    def prettyprint(self):
+        result=self.number3.prettyprint() * self.number4.prettyprint()
+        return result
+    
+class num:
+    def __init__(self,number5):
+        self.number5=number5
+    def prettyprint(self):
+        return int(self.number5)
+
+
+final=mul(num(10),add(num(10),num(20)))
+print(final.prettyprint())
+
+
