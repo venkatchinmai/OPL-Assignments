ec1f4ec024f2712096e769aab4603e8d693d82f0
Chinmai Sai <venkatachinmai@gmail.com>
2019-06-03 01:42:55 -0400

    Completed Task5

---
+++ Task5.py
@@ -0,0 +1,11 @@
+class nodel:
+    def __init__(self,nodelt):
+        self.nodelt=nodelt  
+
+class noder:
+    def __init__(self,nodert):
+        self.nodert=nodert 
+    
+class num:
+    def __init__(self,number):
+        self.number=number
