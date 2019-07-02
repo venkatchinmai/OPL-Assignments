6258176b6d777fe58bf9f5392662e82cf8241397
Chinmai Sai <venkatachinmai@gmail.com>
2019-07-01 22:15:45 -0400

    Task9 done

---
+++ Task9.py
@@ -0,0 +1,31 @@
+#Test cases for J1 programs
+#(iff('>',iff("<",num(7),num(6)),num(8)))
+#if(>,if(<,7,6),8)
+
+#Test cases for J1 programs
+#(iff('>',num(6)),num(8)))
+#if(>,6,8)
+
+#Test cases for J1 programs
+#(iff('>',iff("<",num(99),num(100)),num(101)))
+#if(>,if(<,99,100),101)
+
+#Test cases for J1 programs
+#(iff('>',iff("<",num(111),num(5)),num(7)))
+#if(>,if(<,111,5),7)
+
+#Test cases for J1 programs
+#(iff('>',num(6),num(8)))
+#if(>,if(<,7,6),8)
+
+#Test cases for J1 programs
+#(iff('>',iff("<",num(7),num(6)),num(8)))
+#if(>,if(<,7,6),8)
+
+#Test cases for J1 programs
+#(iff('>',iff("<",num(7),num(6)),num(8)))
+#if(>,if(<,7,6),8)
+
+#Test cases for J1 programs
+#(iff('>',iff("<",num(7),num(6)),num(8)))
+#if(>,if(<,7,6),8)
